87dd77f40f252c897a3eb6346ed94f6d9cc9eb4c
Max Wiktorsson <Maxennnnn@gmail.com>
2022-12-04 23:48:02 +0100

    First side done & removed junk

--- home-bar/tailwind.config.js
+++ home-bar/tailwind.config.js
@@ -1,7 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'light-green': "#87A878",
+        'dark-green': '#313715',
+        'light-red': '#732C2C',
+        'dark-red': '#420C14',
+        'neutral': '#DBD3D8',
+      },
+    },
+  },
+  fontFamily: {
+    'Alata' : ["Alata"]
   },
   plugins: [],
   content: ["./src/**/*.{js,jsx,ts,tsx,html}", "./public/index.html"],
