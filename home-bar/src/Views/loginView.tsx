afbada3aeae373e3c387b89e441ac64adecf15c2
Max Wiktorsson <Maxennnnn@gmail.com>
2022-12-05 15:39:47 +0100

    myIngridients first version done
    -Added loads of dummy data
    -Made scrolling effect for data
    -Added top fixed buttons
    -Added bottom fixed buttons

--- home-bar/src/Views/loginView.tsx
+++ home-bar/src/Views/loginView.tsx
@@ -2,19 +2,27 @@
 
 function loginView() {
     return (
-      <div className="flex flex-col items-center justify-between h-screen w-screen bg-neutral py-[23vh] md:py-[6vh]">
-        <span className="text-2xl text-black">
-          HomeBar
-        </span>
-        <img src="./drink-icon.png" className="w-[65%] md:w-[30%] 2xl:w-[20%]"></img>
-        <a href='#login' className="w-[150px] h-[40px] rounded-2xl bg-light-red text-white text-xl flex justify-center items-center">
-          <span>Log in</span>
-        </a>
-        <a  href='#register' className="flex flex-col justify-center items-center text-lg hover:underline">
-          <span className="-mb-2">Register new</span>
-          <span>account</span>
-        </a>
-      </div>
+        <>
+        {/* MOBILE SCREEN */}
+        <section id="mobileScreen" className="flex flex-col lg:hidden items-center justify-between h-screen w-screen bg-neutral py-[23vh] md:py-[6vh] font-Alata">
+            <span className="text-2xl text-black">
+            HomeBar
+            </span>
+            <img src="./drink-icon.png" className="w-[65%] md:w-[30%] 2xl:w-[20%]"></img>
+            <a href='#login' className="w-[150px] h-[40px] rounded-2xl bg-light-red text-white text-xl flex justify-center items-center">
+            <span>Log in</span>
+            </a>
+            <a  href='#register' className="flex flex-col justify-center items-center text-lg hover:underline">
+            <span className="-mb-2">Register new</span>
+            <span>account</span>
+            </a>
+        </section>
+        
+        {/* DESKTOP SCREEN */}
+        <section id="desktopScreen" className="hidden lg:flex w-screen h-screen justify-center items-center text-3xl font-Alata">
+            PLEASE CHOOSE MOBILE SCREEN
+        </section>
+      </>
     );
   }
   
--- home-bar/src/Views/myIngredientsView.tsx
+++ home-bar/src/Views/myIngredientsView.tsx
@@ -15,27 +15,94 @@ function myIngredientsView() {
     {
       name: 'Jaeger Meister',
     },
+    {
+      name: 'Lemon',
+    },
+    {
+      name: 'Water',
+    },
+    {
+      name: 'Gin',
+    },
+    {
+      name: 'Ice',
+    },
+    {
+      name: 'Absolut Vodka',
+    },
+    {
+      name: 'Candy',
+    },
+    {
+      name: 'Jaeger Meister',
+    },
+    {
+      name: 'Lemon',
+    },
+    {
+      name: 'Water',
+    },
+    {
+      name: 'Gin',
+    },
+    {
+      name: 'Ice',
+    },
+    {
+      name: 'Absolut Vodka',
+    },
+    {
+      name: 'Candy',
+    },
+    {
+      name: 'Jaeger Meister',
+    },
+    {
+      name: 'Lemon',
+    },
+    {
+      name: 'Water',
+    },
+    {
+      name: 'Gin',
+    },
   ]
 
     return (
-      <div className="flex flex-col items-center justify-between h-screen w-screen bg-neutral py-[23vh] md:py-[6vh] font-Alata">
-        <span className="text-2xl text-black">
-          My Ingredients
-        </span>
-        <a href='#login' className="w-[250px] h-[50px] rounded-2xl bg-light-red text-white text-xl flex justify-center items-center">
-          <span>Add new ingredients</span>
-        </a>
-        <div>
+      <>
+      {/* MOBILE SCREEN */}
+      <section id="mobileScreen" className="lg:hidden font-Alata">
+        <div className="bg-neutral fixed w-screen h-[20vh]">
+          <span className="text-2xl text-black flex justify-center items-end h-full pb-8">
+            My Ingredients
+          </span>
+        </div>
+        <div className="fixed mt-[20vh] w-screen h-[10vh] flex justify-center items-center bg-neutral">
+          <a href='#add_igredients' className="w-[250px] h-[50px] rounded-2xl bg-light-red text-white shadow-lg text-xl flex justify-center items-center">
+            <span>Add new ingredients</span>
+          </a>
+        </div>
+        <div className="fixed mt-[85vh] w-screen h-[15vh] flex justify-center items-center bg-neutral">
+          <a href='#Search' className="w-[250px] h-[50px] rounded-3xl bg-dark-red text-white text-xl flex justify-center items-center">
+            <span>Back to Search</span>
+          </a>
+        </div>
+        <div className="flex flex-col items-start w-screen h-fit pb-20 text-xl font-semibold bg-neutral">
+          <div className="mt-[30vh] px-10 pt-8"> 
           {dummyIngredients.map((ingredient) => (
                 <div key={ingredient.name}>{ingredient.name}</div>
 
           ))}
+          </div>
         </div>
-        <a  href='#register' className="flex flex-col justify-center items-center text-lg hover:underline">
-          <span className="-mb-2">Register new</span>
-          <span>account</span>
-        </a>
-      </div>
+        
+      </section>
+
+      {/* DESKTOP SCREEN */}
+      <section id="desktopScreen" className="hidden lg:flex w-screen h-screen justify-center items-center text-3xl font-Alata">
+            PLEASE CHOOSE MOBILE SCREEN
+      </section>
+      </>
     );
   }
   
