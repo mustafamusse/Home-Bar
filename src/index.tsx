e95c39a859155d6aa9bbd4dcba0c9a89d0335858
maxwik <54799665+maxwik@users.noreply.github.com>
2022-12-07 09:14:04 +0100

    Test-push-max

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import React from 'react';
 import cocktailModel from './cocktailModel';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  <React.StrictMode> 
     <App model = {new cocktailModel} />
   </React.StrictMode>
 )
