d5dc4a163376bf0edf8ab652ea526a8767cd2bb2
maxwik <54799665+maxwik@users.noreply.github.com>
2022-12-07 10:11:33 +0100

    Added login navigation

--- src/Views/myIngredientsView.tsx
+++ src/Views/myIngredientsView.tsx
@@ -48,32 +48,44 @@ function myIngredientsView() {
     {
       name: 'Sours',
     },
+    {
+      name: 'Coca Cola',
+    },
+    {
+      name: 'Sprite',
+    },
+    {
+      name: 'Schweppes',
+    },
+    {
+      name: 'Sugar',
+    },
   ]
 
     return (
       <>
       {/* MOBILE SCREEN */}
       <section id="mobileScreen" className="lg:hidden font-Alata">
-        <div className="bg-neutral fixed w-screen h-[20vh]">
+        <div className="bg-neutral fixed w-screen h-[15vh]">
           <span className="text-2xl text-black flex justify-center items-end h-full pb-8">
             My Ingredients
           </span>
         </div>
-        <div className="fixed mt-[20vh] w-screen h-[10vh] flex justify-center items-center bg-neutral">
+        <div className="fixed mt-[15vh] w-screen h-[10vh] flex justify-center items-center bg-neutral border-b-2 border-dark-red">
           <a href='#add_ingredients' className="w-[250px] h-[50px] rounded-2xl bg-light-red text-white shadow-lg text-xl flex justify-center items-center">
             <span>Add new ingredients</span>
           </a>
         </div>
-        <div className="fixed mt-[85vh] w-screen h-[15vh] flex justify-center items-center bg-neutral">
+        <div className="fixed mt-[85vh] w-screen h-[15vh] flex justify-center items-center bg-neutral border-t-2 border-dark-red">
           <a href='#Search' className="w-[250px] h-[50px] rounded-3xl bg-dark-red text-white text-xl flex justify-center items-center">
             <span>Back to Search</span>
           </a>
         </div>
-        <div className="flex flex-col items-start w-screen h-fit pb-20 text-xl font-semibold bg-neutral">
-          <div className="mt-[30vh] px-10 pt-8"> 
+        <div className="flex flex-col items-start w-screen h-fit pb-[20vh] text-xl font-semibold bg-neutral ">
+          <div className="mt-[25vh] px-10 pt-8 w-full"> 
           {dummyIngredients.map((ingredient) => (
-                <div key={ingredient.name}>{ingredient.name}</div>
-
+            <div key={ingredient.name}>{ingredient.name}</div>
+            
           ))}
           </div>
         </div>
