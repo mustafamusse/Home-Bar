5b686139da2897f8da68f7a496450222d5d516db
maxwik <54799665+maxwik@users.noreply.github.com>
2022-12-10 15:25:54 +0100

    Redid buttons on addIngredients

--- src/Components/smallBackButton.tsx
+++ src/Components/smallBackButton.tsx
@@ -4,8 +4,8 @@ export default function SmallBackButton({link, text} : {link:string,text:string}
 
     return(
             <a href={link} className="h-[50px] font-Alata w-fit flex flex-row items-center justify-start gap-2 p-4 transition-transform hover:scale-105 duration-300">
-                <ChevronLeftIcon className="lg:w-[30px] w-[30px] text-black"></ChevronLeftIcon>
-                <p className="lg:text-xl text-xl text-black font-semibold">
+                <ChevronLeftIcon className="lg:w-[30px] w-[30px]"></ChevronLeftIcon>
+                <p className="lg:text-xl text-xl font-semibold">
                     {text}
                 </p>
             </a>
