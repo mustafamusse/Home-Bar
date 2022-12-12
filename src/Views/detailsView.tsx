9293bd9219de357b7256f44031dd73e8b6b0941c
Mustafa Ali <mustali@kth.se>
2022-12-12 20:37:00 +0100

    Detailsview desktop first draft done

--- src/Views/detailsView.tsx
+++ src/Views/detailsView.tsx
@@ -1,4 +1,4 @@
-
+import SmallBackButton from "../Components/smallBackButton";
 
 function DetailView(props : any) {
 
@@ -7,7 +7,10 @@ function DetailView(props : any) {
         <>
         {/* MOBILE SCREEN */}
         <section id="mobileScreen" className="flex flex-col lg:hidden h-screen w-screen bg-neutral font-Alata">
-            <div className="flex flex-col items-center">
+          <div className="bg-neutral fixed w-screen h-fit pt-5">
+            <SmallBackButton text="Search" link="#search"></SmallBackButton>
+          </div>
+            <div className="flex flex-col items-center pt-16">
             <img src="./drink-icon.png" className="w-40 pt-7 pb-3"></img>
             </div>
             <ul className="space-y-1 max-w-md list-disc list-inside text-black dark:text-black pl-2">
@@ -34,8 +37,10 @@ function DetailView(props : any) {
         </section>
         
         {/* DESKTOP SCREEN */}
-        <section id="desktopScreen" className="hidden lg:flex flex-row w-screen h-screen justify-center items-center bg-neutral font-Alata">
-        
+        <section id="desktopScreen" className="hidden lg:flex flex flex-col w-screen h-screen items-center bg-neutral font-Alata">
+        <div className="bg-neutral w-screen pt-5">
+            <SmallBackButton text="Search" link="#search"></SmallBackButton>
+        </div>
         <div className="flex flex-row items-center">
             <img src="./drink-icon.png" className="w-40 pt-7 pb-3"></img>
         </div>
@@ -57,11 +62,11 @@ function DetailView(props : any) {
         </div>
 
         <div className="flex flex-col items-center pt-4">
-          <p className="w-5/6">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat quae sed non impedit voluptas laborum atque deleniti. Quasi ullam, eos velit illo consequuntur quis maiores modi sint esse aspernatur similique?
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat quae sed non impedit voluptas laborum atque deleniti. Quasi ullam, eos velit illo consequuntur quis maiores modi sint esse aspernatur similique?
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat quae sed non impedit voluptas laborum atque deleniti. Quasi ullam, eos velit illo consequuntur quis maiores modi sint esse aspernatur similique?
-          </p>
+        <p className="w-5/6">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto dolores tenetur incidunt reiciendis quaerat omnis ab iure molestias est! Quibusdam cupiditate vel officiis laudantium alias sit, quis quod earum aliquam!
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga porro voluptate voluptas sed reiciendis nesciunt unde at perspiciatis expedita eum reprehenderit, perferendis, qui, corporis fugit impedit! Error voluptate vero distinctio!
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem fugiat saepe sequi minus, quidem unde enim corporis libero est esse, nemo magnam molestiae? Eos repellat quidem, quae omnis quas tempora.
+        </p>
         </div>
 
         </section>
