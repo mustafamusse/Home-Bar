9b0b696f5d699297abdf4a973ec8c945998fc9b1
Mustafa Ali <mustali@kth.se>
2022-12-12 19:50:57 +0100

    Pushing app aswell

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,15 @@ import MyIngredientsPresenter from "./reactjs/myIngredientsPresenter";
 import AddIngredientsPresenter from "./reactjs/addIngredientsPresenter";
 import StartscreenPresenter from "./reactjs/startscreenPresenter";
 import SearchPresenter from "./reactjs/searchPresenter";
+import DetailPresenter from "./reactjs/detailPresenter"
 import Show from "./reactjs/show";
 
 /*VIEWS SHOULD PROBABLY NOT BE RENDERED UNTIL LOGIN IS COMPLETED*/
 function App(props: any) {
   //  return <div style={{"display" : "none"}}><LoginPresenter model={props.model} /></div>
-  return (<div>
+  return (
+  <div>
+    <Show hash="#detail"><DetailPresenter model={props.model} /> </Show>
     <Show hash="#login"><LoginPresenter model={props.model} /> </Show>
     <Show hash="#search"> <SearchPresenter model={props.model} /> </Show>
     <Show hash="#start"> <StartscreenPresenter model={props.model} /> </Show>
@@ -17,5 +20,12 @@ function App(props: any) {
   </div>)
 
 }
+ /*
+    <Show hash="#login"><LoginPresenter model={props.model} /> </Show>
+    <Show hash="#search"> <SearchPresenter model={props.model} /> </Show>
+    <Show hash="#start"> <StartscreenPresenter model={props.model} /> </Show>
+    <Show hash="#ingredients"> <MyIngredientsPresenter model={props.model} /> </Show>
+    <Show hash="#add_ingredients"> <AddIngredientsPresenter model={props.model}/> </Show> 
+    */
 
 export default App;
