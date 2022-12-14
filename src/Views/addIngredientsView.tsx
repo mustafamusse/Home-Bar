964315f4d1ed4a397694a8c1cd511dcc76102d53
maxwik <54799665+maxwik@users.noreply.github.com>
2022-12-14 22:04:50 +0100

    AddIngredients now scrollable!

--- src/Views/addIngredientsView.tsx
+++ src/Views/addIngredientsView.tsx
@@ -24,12 +24,17 @@ function AddIngredientsView(props: any) {
     <>
       <Logo></Logo>
       {/* MOBILE SCREEN */}
-      <section id="mobileScreen" className="lg:hidden font-Alata">
-        <section id="addIngredients" className="fixed w-screen h-screen z-20  flex justify-center items-end">
-          <div className="fixed z-40 w-full h-full bg-black opacity-80"></div>
-          <div className=" text-white fixed w-screen top-0 h-fit pt-5 z-50">
+      <section id="mobileScreen" className="lg:hidden font-Alata bg-neutral">
+        <div className=" text-white fixed w-screen top-0 h-fit pt-5 z-50">
             <SmallBackButton text="My Ingredients" link="#ingredients"></SmallBackButton>
           </div>
+        <div className="fixed z-30 w-full h-full bg-black opacity-80"></div>
+        <div className="w-full h-[20vh] fixed left-0 top-0 flex justify-center z-20">
+          <div className="text-3xl flex justify-center items-end px-2 pb-2 w-fit">
+            <span>My Ingredients</span>
+          </div>
+        </div>
+        <section id="addIngredients" className="fixed w-screen h-screen z-30 flex justify-center items-end">
           <div className=" z-50 bg-dark-green h-[80vh] w-[94vw] rounded-t-[3rem] shadow-2xl  font-medium">
             <div className="h-[6vh] mt-[4vh] mx-8 bg-light-green rounded-[1.3rem] flex justify-between items-center pl-5 pr-2 shadow-xl font-semibold">
               <input onChange={setInputACB} className="bg-light-green w-[80%] h-full font-semibold placeholder-black text-black" type="text" placeholder="Search ingredient:">
@@ -37,15 +42,8 @@ function AddIngredientsView(props: any) {
               <button onClick={searchACB} className="bg-dark-green px-2 py-1 rounded-xl text-white font-medium text-center">Search</button>
             </div>
             <div className="h-[10vh] flex justify-center items-center text-white text-lg underline-offset-[8px] underline z-50 bg-dark-green">Choose Ingridents:</div>
-          </div>
-        </section>
-        <div className="w-screen h-[20vh] fixed left-0 top-0 flex justify-center bg-neutral">
-          <div className="text-3xl flex justify-center items-end px-2 pb-2 w-fit border-b-2  border-dark-red">
-            <span>My Ingredients</span>
-          </div>
-        </div>
-        <div className="flex flex-col items-start w-screen h-fit pb-[10vh] text-xl font-semibold bg-neutral">
-          <div className="mt-[40vh] px-10 pt-8 w-full z-30">
+          <div className="flex flex-col items-start w-full h-[60vh] text-xl font-semibold overflow-auto">
+          <div className=" px-10 pt-8 w-full z-30">
             {searchResultArr.map((ingredient: any) => (
               <div key={ingredient.strIngredient1} className="w-full flex justify-between py-2">
                 <div className="text-white">{ingredient.strIngredient1}</div>
@@ -58,12 +56,14 @@ function AddIngredientsView(props: any) {
                       <div className="w-[30px] h-[30px]">
                         <PlusSmallIcon></PlusSmallIcon>
                       </div>
-                    
                     </button>}
               </div>
             ))}
           </div>
         </div>
+          </div>
+        </section>
+        
 
       </section>
 
