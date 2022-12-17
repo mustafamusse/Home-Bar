3000c04335a1c0700e2df0f01bb8b099a7a02de9
maxwik <54799665+maxwik@users.noreply.github.com>
2022-12-17 02:09:33 +0100

    Made all drink thumbnails the same

--- src/Views/startscreenView.tsx
+++ src/Views/startscreenView.tsx
@@ -13,11 +13,17 @@ function StartscreenView(props: any) {
     }
     //   function chooseDishACB() {props.clickedDish(dish); window.location.hash = "#details"}
     return (<a key={cocktail.idDrink} href="#detail" onClick={itemSelected}>
-        <div className="flex flex-col pt-8 items-center justify-center pr-4">
-            <img src={cocktail.strDrinkThumb} className="h-40 md:h-48 lg:h-60 2xl:h-72 shadow-lg shadow-black"  ></img>
-            <div className="pt-3">{cocktail.strDrink}</div>
-        </div>
-    </a>
+    <div className="flex justify-center pb-4">
+        <div className="flex rounded-2xl w-4/5 bg-dark-green h-40 items-center pl-4">      
+            <img src={cocktail.strDrinkThumb} className="w-100 h-32 shadow-lg shadow-black rounded-2xl text-black"></img>
+            <div className="flex-row pl-4 text-white">
+                <div className="font-bold text-xl underlin">{cocktail.strDrink}</div>
+                <div>{cocktail.strCategory}</div>
+                <div>{cocktail.strAlcoholic}</div>
+            </div>
+            </div>
+    </div>
+</a>
     )
 }
 
@@ -29,7 +35,7 @@ function StartscreenView(props: any) {
             <div className=" fixed w-full h-fit pt-5">
               <SmallBackButton text="Back" link="#login"></SmallBackButton>
             </div>
-            <span className="text-3xl lg:text-5xl xl:text-7xl text-black pt-[13vh]" >
+            <span className="text-3xl lg:text-5xl xl:text-7xl text-black pt-[13vh] font-bold" >
             Welcome!
 
             </span>
@@ -40,11 +46,11 @@ function StartscreenView(props: any) {
               Search for drinks
             </a>
 
-            <span className="text-2xl md:text-3xl lg:text-4xl text-black pt-5 lg:pb-4 text-center">
-            Popular drinks
+            <span className="text-2xl font-bold md:text-3xl lg:text-4xl text-black pt-5 pb-8 lg:pb-4 text-center">
+            Popular drinks:
             </span>
             <div className=" w-full content-center">
-                <div className="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 justify-between"> 
+                <div className="grid grid-cols-1 xl:grid-cols-4 2xl:grid-cols-5 justify-between"> 
                   {props.popularDrinks.map(popDrinksCB)}
                 </div>
             </div>
